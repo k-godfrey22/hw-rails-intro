62218884fd148b674b4358cd9bfb4d6136a0eda6
EC2 Default User <ec2-user@ip-172-31-24-165.us-east-2.compute.internal>
2018-09-28 16:44:29 +0000

    Rotten Potatoes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,62 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.where
+    
+    ##Part 1 (will comment out when it has to be updated for future sections)
+    
+    #@movies = Movie.order(params[:sort_by]) ##we will order using the sort_by param
+    #@sort_column = params[:sort_by] ##columns will be sorted
+    
+    ###################################################################################
+    
+    ##Part 2
+    
+    ##The first thing we need is an all_ratings object we can use in order to get all ratings
+    @all_ratings = Movie.ratings
+    
+    ##next we we want to check if anything is checked if not then our rates will be all ratings
+    ##if it is not nil then it will equal the ratings that are checked (not needed for part3)
+    #params[:ratings].nil? ? @rates = @all_ratings : @rates = params[:ratings].keys
+    
+    
+
+    #to create a sessiont to rembered for the sorting of columns update below line
+    @sort_column = params[:sort_by] || session[:sort_by] ##columns will be sorted and session saved
+    #sort movies when a rating is checked (or not) without breaking functionality of part 1 
+    #@movies = Movie.where(rating: rates).order(params[sort_by]) 
+    
+    
+    ###################################################################################
+    
+    
+    ##Part 3 we wamt to implement sessions that remember where a user was
+    ##possible examples of sessions
+    ##Nothing is checked or sorted, go to more info, come back should be the same
+    ##check box is checked, no column is sorted, go to more info come back, check box is checked, no column is sorted
+    ##check box is checked, column(s) is sorted, go to more info come back, check box is checked, column(s) is sorted
+    ##no check box is checked, column(s) is sorted, go to more info come back, no  check box is checked, column(s) is sorted
+    
+    ##Update some code above to make sessions (example: sorting)
+    
+    ##session for ratings for when we already have a session or @all_ratings exists
+  
+    session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'', 'PG-13'=>'', 'R'=>''}
+    
+    ##Our variable from above, rates, can be updated to include a ratings sessions
+    @rates = params[:ratings] || session[:ratings]
+    
+    session[:sort_by] = @sort_column
+    session[:ratings] = @rates
+    
+    @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort_by]) 
+    
+    ##update Movie.where... line above to remember sessions
+    
+    ##no sort_by parameter and a sort_by session or ratings parameter is nil and no session rating we want to redirect
+    
+    
+    
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+    def self.ratings
+        ##we need to define ratings so that we can use it
+        ##select the didderent ratings that are distinct and put them into our arrays
+        Movie.select(:rating).distinct.inject([]) {|a, m| a.push m.rating}
+    end
+    
 end
